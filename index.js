964cfd17bda1d03033988732f24d7734d74fc458
Bob Maguire <bibo85@yahoo.com>
2025-08-16 06:45:45 -0400

    redo testing chapter

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Reverses a string.
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
@@ -6,15 +5,38 @@ String.prototype.reverse = function() {
 
 // Defines a Phrase object.
 function Phrase(content) {
-  this.content = content;
+  this.content = content; 
 
-  // Returns content processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+  // Method: processor - Returns the passed string processed to lowercase.
+  this.processor = function(string) {
+    return string.toLowerCase();
   }
 
-  // Returns true if the phrase is a palindrome, false otherwise.
-  this.palindrome = function palindrome() {
+  // Method: Returns the content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Method: palindrome - Returns true if the string is a palindrome.
+  this.palindrome = function Palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
+
+  // Method: louder - Returns the content in uppercase.
+  this.louder = function() {
+    return this.content.toUpperCase();
+  }
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Method: Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
